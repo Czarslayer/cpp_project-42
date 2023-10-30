b6e9dff172c439f906ce5af21912ea5fe5ee868e
Mourad Abahani <mabahani@c3r10p6.1337.ma>
2023-10-30 16:11:59 +0100

    final cpp 01 02

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -6,11 +6,13 @@
 /*   By: mabahani <mabahani@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/15 00:06:35 by mabahani          #+#    #+#             */
-/*   Updated: 2023/10/15 00:16:32 by mabahani         ###   ########.fr       */
+/*   Updated: 2023/10/30 16:02:53 by mabahani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <iomanip>
+
 
 int main()
 {
@@ -18,11 +20,12 @@ int main()
     std::string *ptr;
     ptr = &name;
     std::string &ref = name;
-    std::cout << "Value Pointer: " << *ptr << std::endl;
-    std::cout << "Value Reference: " << ref << std::endl;
-    std::cout << "the name: " << name << std::endl;
-    std::cout << "Address of the name: " << &name << std::endl;
-    std::cout << "Address of the pointer: " << ptr << std::endl;
-    std::cout << "Address of the reference: " << &ref << std::endl;
+    std::cout << std::setw(30) << "Address of the name: " << &name << std::endl;
+    std::cout << std::setw(30) << "Address of the pointer: " << ptr << std::endl;
+    std::cout << std::setw(30) << "Address of the reference: " << &ref << std::endl;
+    std::cout << std::endl;
+    std::cout << std::setw(30) << "Value Pointer: " << *ptr << std::endl;
+    std::cout << std::setw(30) << "Value Reference: " << ref << std::endl;
+    std::cout << std::setw(30) << "the name: " << name << std::endl;
     return 0;
 }
