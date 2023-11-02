37c43b00c501fb9d5aec733da786a08239360b69
Mourad Abahani <mabahani@c3r10p10.1337.ma>
2023-11-02 23:52:44 +0100

    testig

---
+++ cpp02/ex01/Fixed.cpp
@@ -0,0 +1,55 @@
+
+#include "Fixed.hpp"
+
+Fixed::Fixed() : _value(0)
+{
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &fixed) {
+    std::cout << "Copy constructor called" << std::endl;
+    *this = fixed;
+}
+
+Fixed::Fixed(const int value) {
+    std::cout << "Int constructor called" << std::endl;
+    _value = value << _bits;
+}
+
+Fixed::Fixed(const float value) {
+    std::cout << "Float constructor called" << std::endl;
+    _value = roundf(value * (1 << _bits));
+}
+
+Fixed::~Fixed() {
+    std::cout << "Destructor called" << std::endl;
+}
+
+Fixed &Fixed::operator=(const Fixed &fixed) {
+    std::cout << "Assignation operator called" << std::endl;
+    _value = fixed.getRawBits();
+    return *this;
+}
+
+std::ostream &Fixed::operator<<(std::ostream &out) {
+    out << fixed.toFloat();
+    return out;
+}
+
+int Fixed::toInt(void) const {
+    return _value >> _bits;
+}
+
+float Fixed::toFloat(void) const {
+    return (float)_value / (1 << _bits);
+}
+
+int Fixed::getRawBits(void) const {
+    std::cout << "getRawBits member function called" << std::endl;
+    return _value;
+}
+
+void Fixed::setRawBits(int const raw) {
+    std::cout << "setRawBits member function called" << std::endl;
+    _value = raw;
+}
---
+++ cpp02/ex01/Fixed.hpp
@@ -0,0 +1,26 @@
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed {
+    public:
+        Fixed();
+        Fixed(const Fixed &fixed);
+        Fixed(const int value);
+        Fixed(const float value);
+        ~Fixed();
+        Fixed &operator=(const Fixed &fixed);
+        std::ostream &operator<<(std::ostream &out, const Fixed &fixed);
+        int getRawBits(void) const;
+        void setRawBits(int const raw);
+        float toFloat( void ) const;
+        int toInt( void ) const;
+    private:
+        int _value;
+        static const int _bits = 8;
+};
+
+#endif
