54e97440745e6e636f31d009ab2b703f593d0174
Mourad Abahani <mabahani@e2r10p9.1337.ma>
2024-04-20 16:01:16 +0100

    final 06

--- cpp06/ex01/main.cpp
+++ cpp06/ex01/main.cpp
@@ -3,16 +3,12 @@
 int main() {
     Serialize s;
     Data data;
-    uintptr_t *raw;
-    Data *ptr;
-
     data.s1 = "Hello";
     data.n = 42;
     data.s2 = "World";
-    raw = s.serialize(&data);
-    // print the raw data
-    std::cout << "raw: " << *raw << std::endl;
-    ptr = s.deserialize(raw);
+    uintptr_t raw = s.serialize(&data);
+    std::cout << "raw: " << raw << std::endl;
+    Data *ptr = s.deserialize(raw);
     std::cout << "s1: " << ptr->s1 << std::endl;
     std::cout << "n: " << ptr->n << std::endl;
     std::cout << "s2: " << ptr->s2 << std::endl;
